cd3962372b2648c0e81507629729b2ef490a592f
Faisal Ali <fai28683@gmail.com>
2018-08-21 13:32:35 +0100

    Option to disable timeout
    Added option to disable the timeout, incase a command is not dependent on timeout.

--- pkg/bash/runBash.go
+++ pkg/bash/runBash.go
@@ -9,14 +9,18 @@ import (
 )
 
 // Wrapper to execute bash command, this will take in the below arguments.
-// timeout: timeout value in ( sections ) which is the maximum time it waits before terminating the process
+// timeout: timeout value in ( sections ) which is the maximum time it waits before terminating the process ( 0 to disable )
 // command: the bash command that it needs to execute
 // args: the arguments the needs to be used along with the command.
 // eg.s RunBashCmd(5, "ping", "-c25", "8.8.8.8")
 // this will return the result of the output and a error if it falls into any
 func RunBashCmd(timeout int, command string, args ...string) (string, error) {
 
-	log.Debugf("Executing the command: %s %v", command, args)
+	// Check if the command executable exists
+	_, err :=  exec.LookPath(command)
+	if err != nil {
+		return "", err
+	}
 
 	// instantiate new command
 	cmd := exec.Command(command, args...)
@@ -32,13 +36,19 @@ func RunBashCmd(timeout int, command string, args ...string) (string, error) {
 		return "" , fmt.Errorf("cmd.Start() error: %s", err.Error())
 	}
 
+	// now we have the command started
+	// the process number for this process is
+	process := cmd.Process
+	pid := process.Pid
+	log.Debugf("Executing the command: %s %v, Process: %d", command, args, pid)
+
 	// Start a ticker to capture the time that it waiting
 	// only good for debugging.
 	ticker := time.NewTicker(time.Second)
 	go func(ticker *time.Ticker) {
 		now := time.Now()
 		for _ = range ticker.C {
-			log.Debugf("Command: %s %v, waiting: %s", command, args, []byte(fmt.Sprintf("%s", time.Since(now))))
+			log.Debugf("Waiting on process (%d): %s", pid, []byte(fmt.Sprintf("%s", time.Since(now))))
 		}
 	}(ticker)
 
@@ -54,11 +64,20 @@ func RunBashCmd(timeout int, command string, args ...string) (string, error) {
 		done <- cmd.Wait()
 	}()
 
-	// block on select, and switch based on actions received
-	select {
+	// If timeout is disabled then wait for the command to finish.
+	if timeout == 0 {
+		err := <-done
+		if err != nil {
+			return "", err
+		}
+		// Done, send the information back to user
+		return buf.String(), nil
+	} else {
+		// block on select, and switch based on actions received
+		select {
 		// Terminate the process since the timeout has reached.
 		case <-time.After(time.Duration(timeout) * time.Second):
-			if err := cmd.Process.Kill(); err != nil {
+			if err := process.Kill(); err != nil {
 				return "", fmt.Errorf("failed to kill: %s", err.Error())
 			}
 			return "", fmt.Errorf("timeout reached, process killed")
@@ -69,6 +88,9 @@ func RunBashCmd(timeout int, command string, args ...string) (string, error) {
 				return "", fmt.Errorf("process done, with error: %s", err.Error())
 			}
 			return buf.String(), nil
+		}
 	}
+
+	// Should never reach here.
 	return "", nil
 }
